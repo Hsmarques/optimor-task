1e02a55b824f761d299dae8cb340a9725aef0067
Hsmarques <mrhsmar@gmail.com>
2016-03-23 17:29:08 +0000

    Printing
    Prices by landline for each country

--- optimor-task.py
+++ optimor-task.py
@@ -1,16 +1,27 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-browser = webdriver.Firefox() # Get local session of firefox
-browser.get("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk") # Load page
-assert "O2" in browser.title
-elem = browser.find_element_by_class_name("o2body") # Find the query box
-elem.send_keys("seleniumhq" + Keys.RETURN)
-time.sleep(1) # Let the page load, will be added to the API
-try:
-    browser.find_element_by_id("showAllCountries")
-except NoSuchElementException:
-    assert 0, "can't find the element you're looking for"
-browser.close()
+driver = webdriver.Firefox()
+driver.implicitly_wait(10) # seconds
+driver.get("http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk")
+
+countries = ['Canada', 'Germany', 'Iceland', 'Pakistan', 'Singapore', 'South Africa']
+
+def get_prices(name):
+	pass
+	element = driver.find_element_by_id("countryName")
+	element.send_keys(name, Keys.RETURN)
+
+	paymonthly = driver.find_element_by_id("paymonthly")
+	paymonthly.send_keys(Keys.RETURN)
+
+	landline = driver.find_element_by_xpath("//table[@id='standardRatesTable']//tbody//td[contains(text(),'Landline')]/following::td")
+	print("Landline: "+landline.text)
+
+for country in countries:
+	print("Prices for "+country)
+	get_prices(country)
+
+print("DONE")
